b749e341b4408fc532ae32eb8fdc9537c852a543
avronilrajib <avronilrajib@gmail.com>
2024-07-29 11:24:02 +0600

    add .env file or server.js

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,12 @@
 const express=require('express');
-
+const dotenv=require('dotenv');
 
 const app=express();
+dotenv.config();
+
+
+app.listen(process.env.PORT,()=>{
+    console.log("server is running");
+})
+
+

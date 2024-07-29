02b7165f86d4a6639bbb001ed6d04960d8f49b09
avronilrajib <avronilrajib@gmail.com>
2024-07-29 12:29:06 +0600

    create chat model

---
+++ backend/models/chatModel.js
@@ -0,0 +1,39 @@
+//chat name
+// is group chat
+// users
+//latest maggase
+//group admin
+const mongoose=require('mongoose');
+
+const chatModel=mongoose.Schema({
+    chatName:{
+        type:String,
+        trim:true
+    },
+    isGroupChat:{
+        type:Boolean,
+        default:false
+    },
+    users:[
+        {
+            type:mongoose.Schema.ObjectId,
+            ref:"User"
+        }
+    ],
+    latestMessage:{
+        type:mongoose.Schema.ObjectId,
+        ref:'Message',
+    },
+    groupAdmin:{
+        type:mongoose.Schema.ObjectId,
+        ref:"User"
+    }
+},
+{
+    timestamps:true,
+}
+);
+
+const chat=mongoose.model('Chat',chatModel);
+
+module.exports=chat;
